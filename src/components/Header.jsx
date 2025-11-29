72e3099f22db9db09e011da4e1258e155fe448ac
ColonelMelnyk <xiaomiredmia15@gmail.com>
2025-11-29 15:06:05 +0200

    header stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,79 @@
+import React from "react";
+import { useSelector } from "react";
+import { selectIsLoggedIn } from "../redux/AuthLogicSelectors"
+import { IoIosSunny, IoIosMoon } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { UserData } from "./UserData";
 const Header = () => {
-  return;
+   const isLoggedIn = useSelector(selectIsLoggedIn);
+   const [darkMode, setDarkMode] = React.useState(
+    localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+  );
+  React.useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [darkMode]);
+
+  const toggleTheme = () => {
+    setDarkMode((prevIsDarkMode) => !prevIsDarkMode);
+  };
+
+
+  return (
+  <nav className="flex items-center py-4 mx-4 *:mx-4 justify-between border-2 border-t-0 border-opacity-80">
+      <div className="*:mx-2 flex items-center">
+        <Link to="/home">
+          <div className="flex">
+            <button className="text-4xl font-bold text-main-yellow">
+              TechSpeed
+            </button>
+          </div>
+        </Link>
+        <Link to="/logistics">
+          <button className="text-[28px] opacity-80 dark:text-white">
+            Logistics
+          </button>
+        </Link>
+        <Link to="/history">
+          <button className="text-[28px] opacity-80 dark:text-white">
+            History
+          </button>
+        </Link>
+      </div>
+      <div className="*:mx-2 flex items-center">
+        {isLoggedIn ? (
+          <UserData/> 
+        ) : (
+          <div className = "flex gap-[8px] flex-col">
+          <Link to="/login">
+            <button className="text-[28px] opacity-80 dark:text-white">
+              Login
+            </button>
+          </Link>
+          <Link to="/register">
+            <button className="text-[28px] opacity-80 dark:text-white">
+              Register
+            </button>
+          </Link> 
+          </div>
+        )}
+        <button onClick={toggleTheme}>
+          {darkMode ? (
+            <IoIosSunny size={32} color="white" />
+          ) : (
+            <IoIosMoon size={32} color="373737" />
+          )}
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -1,5 +1,20 @@
+import { logOut } from 'redux/AuthLogic';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from 'redux/AuthLogicSelectors';
 const UserData = () => {
-  return;
-};
+  const dispatch = useDispatch();
+  const { email, name } = useSelector(selectUser);
 
+  return (
+    <div>
+      <div> 
+        <p  className="text-[28px] opacity-80 dark:text-white">User: {name}</p>
+        <p  className="text-[28px] opacity-80 dark:text-white"> Mail: {email}</p>
+      </div>
+      <button className="text-[28px] opacity-80 dark:text-white" type="button" href=""onClick={() => dispatch(logOut())}>
+        Log Out
+      </button>
+    </div>
+  );
+};
 export default UserData;
