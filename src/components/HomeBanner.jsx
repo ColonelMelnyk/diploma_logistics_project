accf8fde3ab28048257a826987e3b60dd911f7e1
ColonelMelnyk <xiaomiredmia15@gmail.com>
2025-11-29 19:07:00 +0200

    homebanner done

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,5 +1,18 @@
 const HomeBanner = () => {
-  return;
+ 
+  return (
+    <div>
+    <div>
+      <h1>
+        TECHSPEED LOGISTIC SYSTEM
+      </h1>
+      <button type="button" name="order_service">
+        Logistics Center
+      </button>
+    </div>
+  </div>
+  );
+  
 };
 
 export default HomeBanner;
