5687710f390061749f71f827cbd3a0256558d6db
ColonelMelnyk <xiaomiredmia15@gmail.com>
2025-11-29 18:07:48 +0200

    homeinfo added

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import siteInfoData from "../data_storage/SiteInfo";
+
 const HomeInfo = () => {
-  return;
+  return (
+    <section>
+      <h2>Про систему TechSpeed</h2>
+
+      <div>
+        {siteInfoData.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.answer}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default HomeInfo;
