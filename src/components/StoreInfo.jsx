5e98797ab635e2375493a4b7d0f87afa33f84f4b
ColonelMelnyk <xiaomiredmia15@gmail.com>
2025-12-02 21:29:10 +0200

    ok

--- src/components/StoreInfo.jsx
+++ src/components/StoreInfo.jsx
@@ -1,5 +1,15 @@
 const StoreInfo = () => {
-  return;
+  return(
+     <section>
+      <h1>TECHSPEED</h1>
+      <h2>Загальний статус</h2>
+      <div>
+        <p>Комп'ютери: PLACEHOLDER / PLACEHOLDER</p>
+        <p>Телефони та планшети: PLACEHOLDER / PLACEHOLDER</p>
+        <p>Аксесуари: PLACEHOLDER / PLACEHOLDER</p>
+      </div>
+    </section>
+  );
 };
 
 export default StoreInfo;
