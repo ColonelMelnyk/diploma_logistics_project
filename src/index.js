f3f6b090f471b3bc6ba60d9f07c0ec9accd11d08
ColonelMelnyk <xiaomiredmia15@gmail.com>
2026-01-08 23:09:46 +0200

    gh-pages stuff 3

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename="/diploma_logistics_project">
           <App />
         </BrowserRouter>
       </PersistGate>
